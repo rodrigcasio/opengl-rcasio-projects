c65c0c8faeea5afc18f1ef14e1caae74e3336d9d
rodrigcasio <rod.casio@outlook.com>
2026-06-20 00:17:16 -0600

    `feat:` Download and save `glad.c, glad.h` files from glad.dav1d.de

--- 01-creating-a-window/src/main.cpp
+++ 01-creating-a-window/src/main.cpp
@@ -26,9 +26,21 @@ int main() {
     return -1;
   }
   
-  glViewport(0, 0, 800, 600);
-  
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   
   return 0;
+}
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
