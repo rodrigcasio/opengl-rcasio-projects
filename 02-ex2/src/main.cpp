29e2e0758e34b6375d48159fae69ada04a4ffef1
rodrigcasio <rod.casio@outlook.com>
2026-07-11 20:26:48 -0600

    `feat:` Set up data for both triangles `verticesA` & `verticesB` and completed binding first `VAO`, bind `VBO` with its data, and then configure vertex attributes

--- 02-ex2/src/main.cpp
+++ 02-ex2/src/main.cpp
@@ -87,6 +87,41 @@ int main () {
   glDeleteShader(fragmentShader);
 
 
+  /* two triangles (in distinct VAO) */
+  float verticesA[] = {
+    /* left triangle */
+    -0.5f, 0.5f, 0.0f,
+    -0.8f, -0.3f, 0.0f,
+    -0.2f, -0.3f, 0.0f,
+  };
+
+  float verticesB[] = {
+    /* right triangle */
+    0.5f, 0.5f, 0.0f,
+    0.2f, -0.3f, 0.0f,
+    0.8f, -0.3f, 0.0f
+  };
+
+  /* Buffer Objects */
+
+  unsigned int VBO, VBO_2, VAO, VAO_2;
+  glGenVertexArrays(1, &VAO);
+  glGenVertexArrays(2, &VAO_2);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(2, &VBO);
+
+  /* first VBO & VAO for verticesA (first triangle) */
+  glBindVertexArray(VAO); /* ---------------------------- VAO BOUND ----------------------------*/
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verticesA), verticesA, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  
+  glBindVertexArray(0); /* -------------------- UNBIND VAO -------------------------------------*/
 
 
 
