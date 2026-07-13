ec99a0ec0c084c92e87684879785704c4b548018
rodrigcasio <rod.casio@outlook.com>
2026-07-13 17:05:33 -0600

    `feat:` setup `Shaders` chapter directory

--- 03-shaders/src/main.cpp
+++ 03-shaders/src/main.cpp
@@ -1,1 +1,80 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+// Shaders CH
+const unsigned int SRC_WIDTH = 800;
+const unsigned int SRC_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main () {
+  
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+ GLFWwindow* window = glfwCreateWindow(SRC_WIDTH, SCR_HEIGHT, "Shaders CH", NULL, NULL);
+ if (window == NULL) {
+   std::cout << "Failed to create GLFW Window" << std::endl;
+   
+   glfwTerminate();
+   return -1;
+ }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+
+  /* vertex shader */
+  /* frag shader */
+
+  
+  /* buffers */
+  unsigned int VAO, VBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  
+  /* render loop */
+  while (!glfwWindowShouldClose(window)) {
+    /* input */
+    processInput(window);
+
+    /* render */
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, 1); // or true
+  }
+}
+
 
