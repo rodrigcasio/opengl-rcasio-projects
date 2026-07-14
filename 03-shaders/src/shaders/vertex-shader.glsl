387d384491365c95b8ecc3c8f9fe212cc0cd45e9
rodrigcasio <rod.casio@outlook.com>
2026-07-14 17:43:13 -0600

    `feat:` set up an output variable of type `vec4` to set up the color for the fragment shader from the vertex shader

--- 03-shaders/src/shaders/vertex-shader.glsl
+++ 03-shaders/src/shaders/vertex-shader.glsl
@@ -1,10 +1,13 @@
 # version 330 core
-// letting the vertex shader decide the color for the fragment shader
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 aPos; // this position variable has attribute position 0
 
-// out vec4 vertexColor;
+out vec4 vertexColor; // specify a color output to the fragment shader
 
 void main () {
-  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);  // directly give vec3 to vec4 contructor 3 arguments
+  gl_Position = vec4(aPos, 1.0);            // directly give vec3 to vec4 contructor 3 arguments
+  vertexColor = vec4(0.5, 0.0, 0.0, 1.0);   // setting output color to dark-red color
 
 }
+
+
+// letting the vertex shader decide the color for the fragment shader
