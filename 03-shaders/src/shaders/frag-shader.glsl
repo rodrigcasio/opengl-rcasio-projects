2e82aa6c2204fd7e5051d025445f7e48302d22c4
rodrigcasio <rod.casio@outlook.com>
2026-07-14 18:16:20 -0600

    `feat:` Set up uniform variable within the frag-shader file

--- 03-shaders/src/shaders/frag-shader.glsl
+++ 03-shaders/src/shaders/frag-shader.glsl
@@ -1,8 +1,8 @@
 # version 330 core
 out vec4 FragColor;
-
-in vec4 vertexColor;      // input variable from the vertex shader (same name and same type (vec4))
+// in vec4 vertexColor;      // input variable from the vertex shader (same name and same type (vec4))
+uniform vec4 ourColor;      // we set this variable in the OpenGL code
 
 void main () {
-  FragColor = vertexColor;
+  FragColor = ourColor;
 }
