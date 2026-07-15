1abf8e7cecd2d69bd138abc1027d563fe2492807
rodrigcasio <rod.casio@outlook.com>
2026-07-14 19:22:25 -0600

    `feat:` Remove uniform variable, since we no longer use it for the fragments color. Now we use here `ourColor` output varible declared in the vertex shader and place it as an `input` variable for the fragment shader to set up the `FragColor` value

--- 03-shaders/src/shaders/frag-shader.glsl
+++ 03-shaders/src/shaders/frag-shader.glsl
@@ -1,10 +1,11 @@
 # version 330 core
 out vec4 FragColor;
+in vec3 ourColor;
 // in vec4 vertexColor;      // input variable from the vertex shader (same name and same type (vec4))
-uniform vec4 ourColor;      // we set this variable in the OpenGL code
+// uniform vec4 ourColor;      // we set this variable in the OpenGL code
 
 void main () {
-  FragColor = ourColor;
+  FragColor = vec4(ourColor, 1.0);
 }
 
 /* Important: if we declare a uniform that is not used anywhere in your GLSL code, the compiler
