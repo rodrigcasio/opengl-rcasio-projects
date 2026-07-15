024c7d7f5e4cd462ebdf9f8543133c68bcd340e8
rodrigcasio <rod.casio@outlook.com>
2026-07-14 19:18:55 -0600

    `feat:` Adjust the vertex shader to also recieve our color value as a vertex attribute input, setting the location of the `aColor` attribute to 1 with the `layout` specifier

--- 03-shaders/src/shaders/vertex-shader.glsl
+++ 03-shaders/src/shaders/vertex-shader.glsl
@@ -1,12 +1,17 @@
 # version 330 core
 layout (location = 0) in vec3 aPos; // this position variable has attribute position 0
+layout (location = 1) in vec3 aColor;   // the color variable has attribute position 1
 
-out vec4 vertexColor; // specify a color output to the fragment shader
+out vec3 ourColor;   // output a color to the fragment shader 
+
+// out vec4 vertexColor; // specify a color output to the fragment shader
 
 void main () {
-  gl_Position = vec4(aPos, 1.0);            // directly give vec3 to vec4 contructor 3 arguments
-  vertexColor = vec4(0.5, 0.0, 0.0, 1.0);   // setting output color to dark-red color
+  gl_Position = vec4(aPos, 1.0);      // directly give vec3 to vec4 contructor 3 arguments
+  ourColor = aColor;                  // set ourColor to the input color we got from the vertex data
 
+
+  // vertexColor = vec4(0.5, 0.0, 0.0, 1.0);   // setting output color to dark-red color
 }
 
 
