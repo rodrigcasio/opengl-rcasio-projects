1f08fdab6e416a4465fd94902802fad0f85049db
rodrigcasio <rod.casio@outlook.com>
2026-07-15 20:09:45 -0600

    `feat:` first way to adjust triangle upside down

--- 03-ex1/src/main.cpp
+++ 03-ex1/src/main.cpp
@@ -41,7 +41,7 @@ int main () {
     /* x     y      z       R      G     B   */
      0.5f, 0.5f, 0.0f,    1.0f, 0.0f, 0.0f,  // top right
     -0.5f, 0.5f, 0.0f,    0.0f, 1.0f, 0.0f,  // top left
-     0.0f, 0.5f, 0.0f,    0.0f, 0.0f, 1.0f  // bottom middle
+     0.0f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f  // bottom middle (can be changed here or in aPos.y [vertex-shader.glsl])
   };
 
   /* buffers */
