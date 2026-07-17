5f9b282de100401c995a64f206aa61b8497a0884
rodrigcasio <rod.casio@outlook.com>
2026-07-16 18:40:11 -0600

    `feat:` add up the value of the uniform variable `xOffset`

--- 03-ex2/src/shaders/vertex-shader.glsl
+++ 03-ex2/src/shaders/vertex-shader.glsl
@@ -4,8 +4,9 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 out vec3 ourColor;
+uniform float xOffset;   // add more value (0.3f set up in main()) to aPos.x
 
 void main () {
-  gl_Position = vec4(aPos, 1.0);
+  gl_Position = vec4((aPos.x + xOffset), aPos.y, aPos.z, 1.0); 
   ourColor = aColor;
 }
