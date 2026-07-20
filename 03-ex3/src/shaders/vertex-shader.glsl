b075605d301a1e95bdd8730f3c04e913964c5900
rodrigcasio <rod.casio@outlook.com>
2026-07-20 16:09:27 -0600

    `feat:` Set up output varible for the vertex position to the `frag-shader.glsl`

--- 03-ex3/src/shaders/vertex-shader.glsl
+++ 03-ex3/src/shaders/vertex-shader.glsl
@@ -1,12 +1,15 @@
-// Exericise 2 CH: Shaders
+// Exericise 3 CH: Shaders
 # version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 out vec3 ourColor;
+out vec4 ourPosition;
+
 uniform float xOffset;   // add more value (0.3f set up in main()) to aPos.x
 
 void main () {
   gl_Position = vec4((aPos.x + xOffset), aPos.y, aPos.z, 1.0); 
   ourColor = aColor;
+  ourPosition = gl_Position;
 }
