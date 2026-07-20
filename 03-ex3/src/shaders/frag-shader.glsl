49760559b78c5ac0edbf7abb831349f39b17600b
rodrigcasio <rod.casio@outlook.com>
2026-07-20 16:11:03 -0600

    `feat:` set up input varible to recieve the vec4 position and set it up as the `FragColor` value

--- 03-ex3/src/shaders/frag-shader.glsl
+++ 03-ex3/src/shaders/frag-shader.glsl
@@ -1,10 +1,12 @@
-// Exercise 2 CH: Shaders 
+// Exercise 3 CH: Shaders 
 # version 330 core
 out vec4 FragColor;
-in vec3 ourColor;
+
+in vec4 ourPosition;
+// in vec3 ourColor;
 
 void main () {
-  FragColor = vec4(ourColor, 1.0);
+  FragColor = ourPosition;
 }
 
 
