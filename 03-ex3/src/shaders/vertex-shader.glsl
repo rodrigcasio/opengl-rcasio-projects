ac59a6a67beb6e9aa19edc3ee12ce19862ded101
rodrigcasio <rod.casio@outlook.com>
2026-07-20 18:08:26 -0600

    `feat:` modify size of vector output variable ad comment out unnecessary output variable ourColor

--- 03-ex3/src/shaders/vertex-shader.glsl
+++ 03-ex3/src/shaders/vertex-shader.glsl
@@ -4,12 +4,12 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 out vec3 ourColor;
-out vec4 ourPosition;
+out vec3 ourPosition;
 
 uniform float xOffset;   // add more value (0.3f set up in main()) to aPos.x
 
 void main () {
   gl_Position = vec4((aPos.x + xOffset), aPos.y, aPos.z, 1.0); 
-  ourColor = aColor;
-  ourPosition = gl_Position;
+  // ourColor = aColor;
+  ourPosition = aPos;
 }
