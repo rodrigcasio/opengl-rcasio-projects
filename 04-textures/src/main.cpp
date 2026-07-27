0639932a6de8274767b2c67b1dfcd84c4d03994c
rodrigcasio <rod.casio@outlook.com>
2026-07-27 16:32:51 -0600

    `feat:` Set up EBO for `indices` so both triangles can be drawn EBO re-using vertices

--- 04-textures/src/main.cpp
+++ 04-textures/src/main.cpp
@@ -42,7 +42,7 @@ int main () {
     // two triangles drawn (rectangle)
 
     /* Positions */         /* Colors */        /* texture coords */
-    /* x     y    z */    /* R      G     B */  
+    /* x     y    z */    /* R      G     B */  /*S    T */
     0.5f,  0.5f, 0.0f,      1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right  (0)
     0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right (1)
    -0.5f, -0.5f, 0.0f,      0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left  (2)
@@ -67,16 +67,26 @@ int main () {
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
 
+  /* EBO for indices [] */
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+
   glBindVertexArray(VAO); /* ---- BIND VAO ---- */
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+
+  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
