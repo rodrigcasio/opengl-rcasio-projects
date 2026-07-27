d232e8f10b6c6a9939d394432065276792e19535
rodrigcasio <rod.casio@outlook.com>
2026-07-27 17:40:48 -0600

    `feat:` mixing the resulting texture color with the vertex color by muliplying it

--- 04-textures/src/shaders/frag-shader.glsl
+++ 04-textures/src/shaders/frag-shader.glsl
@@ -9,7 +9,8 @@ in vec2 texCoord;
 uniform sampler2D ourTexSampler;
 
 void main () {
-  FragColor = texture(ourTexSampler, texCoord);
+  // FragColor = texture(ourTexSampler, texCoord);
+  FragColor = texture(ourTexSampler, texCoord) * vec4(ourColor, 1.0); /* mix resulting tex color with the vertex color */
 }
 
 
