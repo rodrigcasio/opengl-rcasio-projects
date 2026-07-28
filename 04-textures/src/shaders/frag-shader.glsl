6680b71fb0bbb324400c6c3847b89e8538a0e4f3
rodrigcasio <rod.casio@outlook.com>
2026-07-27 19:18:19 -0600

    `feat:` Set up mixture of textures

--- 04-textures/src/shaders/frag-shader.glsl
+++ 04-textures/src/shaders/frag-shader.glsl
@@ -6,11 +6,17 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 texCoord;
 
-uniform sampler2D ourTexSampler;
+uniform sampler2D texSampler0;
+uniform sampler2D texSampler1;
 
 void main () {
   // FragColor = texture(ourTexSampler, texCoord);
-  FragColor = texture(ourTexSampler, texCoord) * vec4(ourColor, 1.0); /* mix resulting tex color with the vertex color */
+
+  /* mix resulting tex color with the vertex color */
+  // FragColor = texture(texSampler0, texCoord) * vec4(ourColor, 1.0);   
+
+  /* combination of two texture outputs */
+  FragColor = mix(texture(texSampler0, texCoord), texture(texSampler1, texCoord), 0.2);  
 }
 
 
