0dd475194bf4d0566adedcaa36c0599cf27b1160
rodrigcasio <rod.casio@outlook.com>
2026-07-30 16:52:24 -0600

    `feat:` modify the `x` axis by subtracting by 1.0 the value of the original texture coordinates in the `x = s` axis

--- 04-ex1/src/shaders/frag-shader.glsl
+++ 04-ex1/src/shaders/frag-shader.glsl
@@ -1,4 +1,4 @@
-// Textures CH
+// exercise 1 CH Textures
 # version 330 core
 out vec4 FragColor;
 
@@ -16,7 +16,7 @@ void main () {
   // FragColor = texture(texSampler0, TexCoord) * vec4(ourColor, 1.0);   
 
   /* combination of two texture outputs */
-  FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, vec2(TexCoord.s, TexCoord.t)), 0.2);  
+  FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, vec2(1.0 - TexCoord.s, TexCoord.t)), 0.2);  
 }
 
 
