24d22c6bb30798ea3827cbf3fe1351498a8b4e1b
rodrigcasio <rod.casio@outlook.com>
2026-07-30 17:45:24 -0600

    `fix:` change description comment for exercise 1

--- 04-ex1/src/shaders/frag-shader.glsl
+++ 04-ex1/src/shaders/frag-shader.glsl
@@ -15,7 +15,7 @@ void main () {
   /* mix resulting tex color with the vertex color */
   // FragColor = texture(texSampler0, TexCoord) * vec4(ourColor, 1.0);   
 
-  /* combination of two texture outputs */
+  /* combination of two texture outputs inverting x axis */
   FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, vec2(1.0 - TexCoord.s, TexCoord.t)), 0.2);  
 }
 
