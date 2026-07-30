379d5e5ca0601628d6dfbd4b0f95b17d1d32f696
rodrigcasio <rod.casio@outlook.com>
2026-07-30 15:31:15 -0600

    `feat:` setting up approach for exercise 1

--- 04-ex1/src/shaders/frag-shader.glsl
+++ 04-ex1/src/shaders/frag-shader.glsl
@@ -16,7 +16,7 @@ void main () {
   // FragColor = texture(texSampler0, TexCoord) * vec4(ourColor, 1.0);   
 
   /* combination of two texture outputs */
-  FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, TexCoord), 0.2);  
+  FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, vec2(TexCoord.s, TexCoord.t)), 0.2);  
 }
 
 
