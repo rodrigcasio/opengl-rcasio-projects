1626b976cddd94550619462935669dc616f8283b
rodrigcasio <rod.casio@outlook.com>
2026-08-01 14:25:47 -0600

    `fix:` Typo within line 22 in `src/main.cpp`

--- 04-ex2/src/main.cpp
+++ 04-ex2/src/main.cpp
@@ -19,7 +19,7 @@ int main () {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPACT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 #endif
   
