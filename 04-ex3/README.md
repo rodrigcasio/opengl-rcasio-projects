e4b1b7b9a20c80ef974883df57db90bdd79663cb
rodrigcasio <rod.casio@outlook.com>
2026-08-01 18:54:09 -0600

    `feat:` Set up exercise 3 directory with needed files

---
+++ 04-ex3/README.md
@@ -0,0 +1,6 @@
+
+### Exercise 3 CH: Textures
+
+- `Try to display only the center pixels of the texture image on the rectangle in such a way that the individual pixels are getting visible by changing the texture coordinates. Try to set the texture filtering method to GL_NEAREST to see the pixels more clearly:`
+
+- Result:
---
+++ 04-ex3/src/shaders/vertex-shader.glsl
@@ -0,0 +1,18 @@
+// exercise 3 ch:textures
+# version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 ourColor;
+out vec2 TexCoord;
+// out vec3 ourPosition;
+
+// uniform float xOffset;   // add more value (0.3f set up in main()) to aPos.x
+
+void main () {
+  gl_Position = vec4(aPos, 1.0); 
+  ourColor = aColor;
+  TexCoord = vec2(aTexCoord);
+  // ourPosition = aPos;
+}
