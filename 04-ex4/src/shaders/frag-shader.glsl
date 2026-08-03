87a696f489ddca0cbe376d0361f39238d74407b0
rodrigcasio <rod.casio@outlook.com>
2026-08-03 15:37:00 -0600

    set up uniform for exercise 4 and returned default range of texture coordinates

--- 04-ex4/src/shaders/frag-shader.glsl
+++ 04-ex4/src/shaders/frag-shader.glsl
@@ -8,6 +8,7 @@ in vec2 TexCoord;
 
 uniform sampler2D texSampler0;
 uniform sampler2D texSampler1;
+uniform thirdArg
 
 void main () {
   // FragColor = texture(ourTexSampler, TexCoord);
@@ -16,7 +17,7 @@ void main () {
   // FragColor = texture(texSampler0, TexCoord) * vec4(ourColor, 1.0);   
 
   /* combination of two texture outputs */
-  FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, TexCoord), 0.2);  
+  FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, TexCoord), thirdArg);  
 }
 
 
