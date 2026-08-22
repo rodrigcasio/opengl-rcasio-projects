510359b06afa98da42940989f711fa37bd1ed209
rodrigcasio <rod.casio@outlook.com>
2026-08-22 00:42:14 -0600

    `feat:` Added mix function with new named uniform variable

--- 05-ex2/src/shaders/frag-shader.glsl
+++ 05-ex2/src/shaders/frag-shader.glsl
@@ -7,17 +7,20 @@ in vec3 ourColor;
 in vec2 TexCoord;
 
 uniform sampler2D texSampler0;
-// uniform sampler2D texSampler1;
-// uniform float mixThirdArg;
+uniform sampler2D texSampler1;
+uniform float uMixThirdArg;
 
 void main () {
   // FragColor = texture(ourTexSampler, TexCoord);
 
   /* mix resulting tex color with the vertex color */
-  FragColor = texture(texSampler0, TexCoord) * vec4(ourColor, 1.0);   
+  // FragColor = texture(texSampler0, TexCoord) * vec4(ourColor, 1.0);   
 
   /* combination of two texture outputs */
-  // FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, TexCoord), mixThirdArg);
+  // FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, TexCoord), 0.3);
+
+  /* using mix with third argument as a uniform value */
+  FragColor = mix(texture(texSampler0, TexCoord), texture(texSampler1, TexCoord), uMixThirdArg);
 }
 
 
